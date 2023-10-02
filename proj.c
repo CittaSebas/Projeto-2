16e4d11f653e45d77ea56de05dbdfdec4e7e22d2
Sebastian <cittasebas@gmail.com>
2023-10-02 16:53:53 -0300

    Initial commit

---
+++ proj.c
@@ -0,0 +1,5 @@
+//
+// Created by Sebastian on 02/10/2023.
+//
+
+#include "proj.h"
---
+++ proj.h
@@ -0,0 +1,8 @@
+//
+// Created by Sebastian on 02/10/2023.
+//
+
+#ifndef PROJETO_2_PROJ_H
+#define PROJETO_2_PROJ_H
+
+#endif //PROJETO_2_PROJ_H
