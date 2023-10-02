06bb1b173153eec6f34dc85ae61e7fb098b3d016
Sebastian <cittasebas@gmail.com>
2023-10-02 16:57:44 -0300

    Menu
    Menu Basico

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Menu:\n");
+    printf("1. Novo Cliente \n");
+    printf("2. Apaga Cliente \n");
+    printf("3. Listar  \n");
+    printf("4. Debito \n");
+    printf("5. Deposito \n");
+    printf("6. Extrato \n");
+    printf("7. Transferencia entre Contas \n");
+    printf("0. Sair \n");
     return 0;
 }
