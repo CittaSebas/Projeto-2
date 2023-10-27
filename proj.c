7ed641df40477d903ab25a4ed599be6d53d4c168
JuanCitta <143374068+JuanCitta@users.noreply.github.com>
2023-10-27 13:23:03 -0300

    Criação da função para salvar e carregar a lista em um arquivo e protótipo não funcional da função ListarClientes

--- proj.c
+++ proj.c
@@ -15,7 +15,7 @@ int criarCliente(ListaDeClientes *lc){
     //
 
     // Abrindo o arquivo
-    FILE *f = fopen("arquivo.bin", "wb");
+    FILE *f = fopen("arquivo.bin", "ab");
     //
 
     // Se o Arquivo não é encontrado retorna um código de erro
@@ -35,13 +35,18 @@ int criarCliente(ListaDeClientes *lc){
     scanf("%s",novocliente.cpf);
 
     printf("Que tipo de conta gostaria (Comum ou Plus): \n");
-    scanf("%d",&novocliente.tipodeconta);
+    scanf("%s",&novocliente.tipodeconta);
 
     printf("Qual sera o Valor Inicial depositado: \n");
-    scanf("%d",&novocliente.valorinicial);
+    scanf("%f",&novocliente.valorinicial);
 
     printf("Escreva sua Senha: \n");
-    scanf("%d",&novocliente.senha);
+    scanf("%s",&novocliente.senha);
+    //
+
+    // Contando o número de tarefas
+    lc->clientes[lc->qtd] = novocliente;
+    lc->qtd++;
     //
 
     return 0;
@@ -58,8 +63,76 @@ void printMenu(){
 
 }
 
-int salvarLista(ListaDeClientes lc, char nome[]){return 0;}
-int carregarLista(ListaDeClientes *lc,char nome[]){return 1;}
+int salvarLista(ListaDeClientes lc, char nome[]){ // Abrindo o arquivo no modo WB
+    FILE *f= fopen("arquivo.bin", "wb");
+    //
+
+    if(f==NULL){
+        return 1;
+    }
+    // Escrevendo a lista atualizada no arquivo
+    fwrite(&lc, sizeof(ListaDeClientes), 1, f);
+    //
+
+    // Fechando o arquivo
+    fclose(f);
+    //
+
+    // Indicação de Sucesso
+    return 0;}
+//}
+int carregarLista(ListaDeClientes *lc,char nome[]){   // Abrindo o arquivo como read
+    FILE *f = fopen("arquivo.bin", "rb");
+    //
+
+    if(f==NULL)
+        return 1;
+
+    //Comando para leitura de cada Tarefa dentro da ListaDeTarefas no endereço de lt
+    fread(lc,sizeof(ListaDeClientes ),1,f);
+    //
+
+    //Fecha o arquivo
+    fclose(f);
+    //
+
+    // Indicação de Sucesso
+    return 0;}
+
+//int listarClientes(ListaDeClientes lc) {
+//    int i = 0;
+//
+//    // Mensagem para o usuário
+//    printf("Clientes: \n");
+//    //
+//
+//    //Loop de repetição para leitura de cada Tarefa dentro da ListaDeTarefas no endereço lt
+//    while (i < lc.qtd) {
+//        printf("-----------------------\n");
+//        printf("Numero da Tarefa: %d\n", i);
+//        printf("Categoria: %s\n", lc.clientes[i].nome);
+//        printf("Descricao: %s\n", lc.clientes[i].cpf);
+//        printf("Prioridade: %s\n", lc.clientes[i].tipodeconta);
+//        printf("Prioridade: %d\n", lc.clientes[i].valorinicial);
+//        i++;
+//    }
+//    return 0;
+//    //
+//}
+
+int listarClientes(ListaDeClientes lc) {
+    printf("Total number of clients: %d\n", lc.qtd);
+
+    for (int i = 0; i < lc.qtd; i++) {
+        printf("-----------------------\n");
+        printf("Numero da Tarefa: %d\n", i);
+        printf("Categoria: %s\n", lc.clientes[i].nome);
+        printf("Descricao: %s\n", lc.clientes[i].cpf);
+        printf("Prioridade: %s\n", lc.clientes[i].tipodeconta);
+        printf("Prioridade: %d\n", lc.clientes[i].valorinicial);
+    }
+    return 0;
+}
 
 //#include "proj.h"
 //#include <stdio.h>
@@ -153,5 +226,3 @@ int carregarLista(ListaDeClientes *lc,char nome[]){return 1;}
 //}
 //
 //void printMenu(){ printf("Menu:\n");}
-//int salvarLista(ListaDeTarefas lt, char nome[]){return 0;}
-//int carregarLista(ListaDeTarefas *lt,char nome[]){return 1;}
